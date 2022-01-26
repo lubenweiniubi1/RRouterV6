021557e5b0170c7755b3d4f315fb6c42feb9fb98
lubenweiniubi1 <631912579@qq.com>
2022-01-26 17:47:26 +0800

    3 files modified

--- src/routes/invoices.jsx
+++ src/routes/invoices.jsx
@@ -1,9 +1,15 @@
-import { NavLink, Outlet, useSearchParams } from 'react-router-dom';
+import {
+  NavLink,
+  Outlet,
+  useLocation,
+  useSearchParams,
+} from 'react-router-dom';
 import { getInvoices } from '../data';
 
 export default function Invoices() {
   let invoices = getInvoices();
   let [searchParams, setSearchParams] = useSearchParams();
+  let location = useLocation();
   console.log(searchParams.get('filter'));
   return (
     <div style={{ display: 'flex' }}>
@@ -25,19 +31,26 @@ export default function Invoices() {
             }
           }}
         />
-        {invoices.map((invoice) => (
-          <NavLink
-            style={({ isActive }) => ({
-              display: 'block',
-              margin: '1rem 0',
-              color: isActive ? 'red' : '',
-            })}
-            key={invoice.number}
-            to={`/invoices/${invoice.number}`}
-          >
-            {invoice.name}
-          </NavLink>
-        ))}
+        {invoices
+          .filter((invoice) => {
+            let filter = searchParams.get('filter');
+            if (!filter) return true;
+            let name = invoice.name.toLowerCase();
+            return name.startsWith(filter.toLowerCase());
+          })
+          .map((invoice) => (
+            <NavLink
+              style={({ isActive }) => ({
+                display: 'block',
+                margin: '1rem 0',
+                color: isActive ? 'red' : '',
+              })}
+              key={invoice.number}
+              to={`/invoices/${invoice.number}${location.search}`}
+            >
+              {invoice.name}
+            </NavLink>
+          ))}
       </nav>
       <Outlet />
     </div>
